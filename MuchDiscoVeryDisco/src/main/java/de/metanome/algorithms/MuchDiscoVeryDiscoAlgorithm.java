057ef7c359e4507f03b5d6b6ad4769991911d0bd
Manuel Zedel <manuel.zedel@live.de>
2014-11-20 09:42:53 +0100

    MuchDiscoVeryDisco added

---
+++ MuchDiscoVeryDisco/src/main/java/de/metanome/algorithms/MuchDiscoVeryDiscoAlgorithm.java
@@ -0,0 +1,14 @@
+package de.metanome.algorithms;
+
+import de.metanome.algorithm_integration.AlgorithmExecutionException;
+
+public class MuchDiscoVeryDiscoAlgorithm {
+
+  public void execute() throws AlgorithmExecutionException {
+    
+  }
+
+  public String toString() {
+    return this.getClass().getName();
+  }
+}
