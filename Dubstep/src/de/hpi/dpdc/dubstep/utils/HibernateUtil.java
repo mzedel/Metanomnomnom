cad738634020ff3c6125e0f31ba8514467aed174
Tim Sporleder <tim.sporleder@student.hpi.uni-potsdam.de>
2015-01-19 16:20:33 +0100

    some cleaning, some converting

--- Dubstep/src/de/hpi/dpdc/dubstep/utils/HibernateUtil.java
+++ Dubstep/src/de/hpi/dpdc/dubstep/utils/HibernateUtil.java
@@ -8,31 +8,32 @@ import org.hibernate.service.ServiceRegistry;
 import de.hpi.dpdc.dubstep.detection.address.Address;
 
 public class HibernateUtil {
-  
-  private static SessionFactory sessionFactory;
-  private static ServiceRegistry serviceRegistry;
-  
-  public static SessionFactory buildSessionFactory() {
-    Configuration config = new Configuration()
-      .addAnnotatedClass(Address.class)
-      .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
-      .setProperty("hibernate.connection.url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
-      .setProperty("hibernate.current_session_context_class", "thread")
-      .setProperty("hibernate.show_sql", "true")
-      .setProperty("hibernate.hbm2ddl.auto", "create");
-    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-    return config.buildSessionFactory(serviceRegistry);
-  }
 
-  public static SessionFactory getSessionFactory() {
-    if (sessionFactory == null)
-      sessionFactory = buildSessionFactory();
-    return sessionFactory;
-  }
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
 
-  public static void shutdown() {
-    // Close caches and connection pools
-    getSessionFactory().close();
-  }
-  
+	public static SessionFactory buildSessionFactory() {
+		Configuration config = new Configuration()
+		.addAnnotatedClass(Address.class)
+		.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
+		.setProperty("hibernate.connection.url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
+		.setProperty("hibernate.current_session_context_class", "thread")
+		.setProperty("hibernate.show_sql", "true")
+		.setProperty("hibernate.hbm2ddl.auto", "create");
+		serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+		return config.buildSessionFactory(serviceRegistry);
+	}
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			sessionFactory = buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		// Close caches and connection pools
+		getSessionFactory().close();
+	}
+
 }
